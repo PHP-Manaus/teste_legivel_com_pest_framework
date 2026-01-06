a577b6c6c990381f41b4e0044576d0dc999b4c97
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2026-01-06 01:41:12 -0400

    refactor: extrair constante para status padrao

--- app/Models/Bug.php
+++ app/Models/Bug.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bug extends Model
 {
+    public const ABERTO = 'aberto';
+
     public static function statusPadrao(): string
     {
-        return 'aberto';
+        return self::ABERTO;
     }
 }
